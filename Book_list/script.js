dd959baa49e02b05b72e6c34a25bfd904ab2df12
MD All Mubin <mdaj6804@gmail.com>
2023-01-20 11:53:12 +0600

    All Complited BookList Progect

--- Book_list/script.js
+++ Book_list/script.js
@@ -8,7 +8,7 @@ let book_list = document.querySelector("#book_list")
 
 
 
-console.log(form)
+// console.log(form)
 
 
 
@@ -24,38 +24,91 @@ class Book {
 }
 //UI CLASS 
 class UI {
-    constructor() {
 
-    }
-    addToBooklist(book) {
+    static addToBooklist(book) {
         let row = document.createElement("tr");
         row.innerHTML = `
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.isbn} </td>
-        <td><a href="#" class="delete"> X</a></td>`;
+        <td><a  class="delete"><i href="#" class="fa-solid fa-xmark"></i></a></td>`;
         book_list.appendChild(row);
         // console.log(row)
 
     }
-    clearFields() {  //Input জায়গা ক্লিয়ার এর জন্য
+    static clearFields() {  //Input জায়গা ক্লিয়ার এর জন্য
         document.querySelector("#title").value = "";
         document.querySelector("#author").value = "";
         document.querySelector("#isbn").value = "";
 
     }
+    static showAlert(message, ClassName) {
+        let div = document.createElement("div");
+        div.className = `alert ${ClassName}`;
+        div.appendChild(document.createTextNode(message));
+        let container = document.querySelector(".container");
+        container.insertBefore(div, form);
+        setTimeout(() => {
+            document.querySelector(".alert").remove();
+        }, 2000)
+    }
+    static deleleFromBooK(target) {
+        if (target.hasAttribute("href")) {
+
+            target.parentElement.parentElement.parentElement.remove()
+            // store.removeBookmark
+           
+            store.removeBook(target.parentElement.parentElement.previousElementSibling.textContent.trim());
+                UI.showAlert("Book Removed!", "success");
+
+        }
+
+    }
 }
+// localStorage class
+class store {
+    static getBooks() {
+        let books;
+        if (localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+    static addBook(book) {
+        let books = store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+    static displayBools() {
+        let books = store.getBooks();
 
+        books.forEach(book => {
+            UI.addToBooklist(book)
 
+        });
+    }
+    static removeBook(isbn) {
+        let books = store.getBooks();
+        books.forEach((book, index) => {
+            if (book.isbn === isbn) {
+                books.splice(index, 1);
 
+            }
+        });
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+}
 
 
 
 
 //add event listener
 form.addEventListener("submit", newBook)
-
-
+book_list.addEventListener("click", removeBook);
+document.addEventListener("DOMContentLoaded", store.displayBools())
 
 
 
@@ -65,17 +118,29 @@ form.addEventListener("submit", newBook)
 
 function newBook(e) {
     // console.log(e);
-        let title = document.querySelector("#title").value,
-            author = document.querySelector("#author").value,
-            isbn = document.querySelector("#isbn").value;
+    let title = document.querySelector("#title").value,
+        author = document.querySelector("#author").value,
+        isbn = document.querySelector("#isbn").value;
+
+
+    if (title === "" || author === "" || isbn === "") {
+        UI.showAlert("please fill all the fields!", "error");
+    } else {
         let book = new Book(title, author, isbn);
-        let ui = new UI();
-        ui.addToBooklist(book);
-        ui.clearFields()//Input জায়গা ক্লিয়ার এর জন্য
+        UI.addToBooklist(book);
+        UI.clearFields();//Input জায়গা ক্লিয়ার এর জন্য
+        UI.showAlert("Book Added", "success");
+        store.addBook(book);
+    }
     e.preventDefault() //Browser যে না রিলড হয় সে জন্য
+
 
 }
 
 
 
+function removeBook(e) {
 
+    UI.deleleFromBooK(e.target)
+    e.preventDefault();
+}
